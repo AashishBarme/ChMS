755df67b36c2af4a987df596a83d2bf72ec51181
AashishBarme <aashish.barme9@gmail.com>
2024-05-20 10:10:00 +0545

    added new ui design and folder structure for frontend

--- Frontend/web-ui/src/app/app.module.ts
+++ Frontend/web-ui/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
+import { AuthModule } from './modules/auth/auth.module';
+import { InventoryModule } from './modules/inventory/inventory.module';
+import { MemberModule } from './modules/member/member.module';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule,
+    InventoryModule,
+    MemberModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Frontend/web-ui/src/app/modules/inventory/inventory-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AddComponent } from './add/add.component';
+import { ListComponent } from './list/list.component';
+import { EditComponent } from './edit/edit.component';
+
+const routes: Routes = [{
+  "path" : "inventory",
+  "children" :
+  [
+      {"path" : "add" , component:AddComponent},
+      {"path" : "list" , component:ListComponent},
+      {"path" : "edit/:id" , component:EditComponent}
+  ]
+}];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class InventoryRoutingModule { }
