acae8938b8b71bc96209f78d6575418fddf1bf0c
AashishBarme <aashish.barme9@gmail.com>
2024-05-20 11:05:18 +0545

    update layout content

--- Frontend/web-ui/src/app/modules/member/member.module.ts
+++ Frontend/web-ui/src/app/modules/member/member.module.ts
@@ -5,6 +5,11 @@ import { MemberRoutingModule } from './member-routing.module';
 import { AddComponent } from './add/add.component';
 import { EditComponent } from './edit/edit.component';
 import { ListComponent } from './list/list.component';
+import { LayoutModule } from '../layout/layout.module';
+// import { HeaderComponent } from 'src/app/components/shared/header/header.component';
+// import { SidebarComponent } from 'src/app/components/shared/sidebar/sidebar.component';
+// import { BreadcrumbComponent } from 'src/app/components/shared/breadcrumb/breadcrumb.component';
+// import { FooterComponent } from 'src/app/components/shared/footer/footer.component';
 
 
 @NgModule({
@@ -15,7 +20,8 @@ import { ListComponent } from './list/list.component';
   ],
   imports: [
     CommonModule,
-    MemberRoutingModule
+    MemberRoutingModule,
+    LayoutModule
   ]
 })
 export class MemberModule { }
