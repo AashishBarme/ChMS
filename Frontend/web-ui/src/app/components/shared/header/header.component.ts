89d984fe329e10ebaac32c11ee0c1d8e232ba710
AashishBarme <aashish.barme9@gmail.com>
2024-07-16 23:42:39 +0545

    added date time in header

--- Frontend/web-ui/src/app/components/shared/header/header.component.ts
+++ Frontend/web-ui/src/app/components/shared/header/header.component.ts
@@ -1,19 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   userName: string = "User";
+  time: string = '';
+  date: string = '';
+  private timerId: any;
   constructor() { }
 
   ngOnInit(): void {
+    this.updateTime();
+    this.timerId = setInterval(() => this.updateTime(), 1000);
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.timerId);
+  }
+
+  private updateTime(): void {
+    const now = new Date();
+    this.time = now.toLocaleTimeString();
+    this.date = now.toLocaleDateString();
   }
 
   logOut(){
-    console.log("This is working");
+      localStorage.clear();
+      window.location.replace("/");
   }
 
 }
