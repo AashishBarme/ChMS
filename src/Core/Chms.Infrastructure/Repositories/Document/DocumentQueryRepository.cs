26f9ad089ca26941e470fa23e904b31803954bfd
AashishBarme <aashish.barme9@gmail.com>
2024-07-30 22:18:14 +0545

    update date filter query for document list

--- src/Core/Chms.Infrastructure/Repositories/Document/DocumentQueryRepository.cs
+++ src/Core/Chms.Infrastructure/Repositories/Document/DocumentQueryRepository.cs
@@ -26,12 +26,12 @@ namespace Chms.Infrastructure.Repositories.Document
         public int GetTotalData(FilterVm query)
         {
             var whereSql = (!String.IsNullOrEmpty(query.Name)) ? $" and Name like @name" : string.Empty;
-            whereSql += (!String.IsNullOrEmpty(query.CreatedDate)) ? $" and CreatedDate = @date" : string.Empty;
+            whereSql += (!String.IsNullOrEmpty(query.CreatedDate)) ? $" and CAST(CreatedDate as date) = @date" : string.Empty;
             var totalDataSql = $"select COUNT(Id) as TotalCount from {TABLE_NAME} where 1 = 1" + whereSql;
             object where = new
             {
                 name = $"%{query.Name}%",
-                date = $"%{query.CreatedDate}%",
+                date = query.CreatedDate,
             };
             return _baseRepository.LoadSingleData<int, object>(totalDataSql, where).GetAwaiter().GetResult();
         }
@@ -40,13 +40,13 @@ namespace Chms.Infrastructure.Repositories.Document
         {
             string sql = $"select * from {TABLE_NAME} where 1 = 1 ";
             var whereSql = (!String.IsNullOrEmpty(query.Name)) ? $" and Name like @name" : string.Empty;
-            whereSql += (!String.IsNullOrEmpty(query.CreatedDate)) ? $" and CreatedDate = @date" : string.Empty;
+            whereSql += (!String.IsNullOrEmpty(query.CreatedDate)) ? $" and CAST(CreatedDate as date) = @date" : string.Empty;
             sql += whereSql;
             sql += $" order by CreatedDate desc OFFSET {query.Offset} ROWS FETCH NEXT {query.Limit} ROWS ONLY";
             object where = new
             {
                 name = $"%{query.Name}%",
-                date = $"%{query.CreatedDate}%"
+                date = query.CreatedDate
             };
             return _baseRepository.LoadData<Domain.Entities.Document, object>(sql, where).GetAwaiter().GetResult();
         }
