1db06b2768ca15f71c206627b53c198ff0ccc133
AashishBarme <aashish.barme9@gmail.com>
2024-09-03 20:27:51 +0545

    added expense component html

--- Frontend/web-ui/src/app/modules/finance/add-expense/add-expense.component.ts
+++ Frontend/web-ui/src/app/modules/finance/add-expense/add-expense.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AddIncome, Income } from '../finance.model';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-expense',
@@ -6,5 +9,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-expense.component.css']
 })
 export class AddExpenseComponent {
+  pageParentLink = 'Inventory';
+  pageTitle = 'Add Item';
+  model: AddIncome = new AddIncome;
+  income: Income = new Income;
+  categories: string[] = ["Offering","Bonus","Interest","Personal Help","Mission","Others"];
+  tithes: Income[] = [];
 
+  constructor(
+    private _router: Router,
+    private toastr: ToastrService
+  ){
+    for(let i = 0; i< this.categories.length; i++)
+    {
+      let income = new Income;
+      income.category = this.categories[i];
+      this.model.income.push(income);
+    }
+  }
+
+
+
+  ngOnInit(): void {
+    // this.incomeForm = this.fb.group
+  }
+
+  onSubmit():void{
+    for(let i = 0; i< this.tithes.length; i++)
+    {
+      this.model.income.push(this.tithes[i])
+    }
+    console.log(this.model);
+  }
 }
--- Frontend/web-ui/src/app/modules/finance/edit-expense/edit-expense.component.ts
+++ Frontend/web-ui/src/app/modules/finance/edit-expense/edit-expense.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AddIncome, Income } from '../finance.model';
 @Component({
   selector: 'app-edit-expense',
   templateUrl: './edit-expense.component.html',
   styleUrls: ['./edit-expense.component.css']
 })
 export class EditExpenseComponent {
+  pageParentLink = 'Inventory';
+  pageTitle = 'Add Item';
+  model: AddIncome = new AddIncome;
+  income: Income = new Income;
+  categories: string[] = ["Offering","Bonus","Interest","Personal Help","Mission","Others"];
+  tithes: Income[] = [];
 
+  constructor(
+    private _router: Router,
+    private toastr: ToastrService
+  ){
+    for(let i = 0; i< this.categories.length; i++)
+    {
+      let income = new Income;
+      income.category = this.categories[i];
+      this.model.income.push(income);
+    }
+  }
+
+
+
+  ngOnInit(): void {
+    // this.incomeForm = this.fb.group
+  }
+
+  onSubmit():void{
+    for(let i = 0; i< this.tithes.length; i++)
+    {
+      this.model.income.push(this.tithes[i])
+    }
+    console.log(this.model);
+  }
 }
