85fa4f32f5aabf053a083a7f41b740ceb8da8335
AashishBarme <aashish.barme9@gmail.com>
2024-09-07 23:25:22 +0545

    Added finance repo and service interface

--- src/Core/Chms.Application/Common/Interface/Repositories/IFinanceRepository.cs
+++ src/Core/Chms.Application/Common/Interface/Repositories/IFinanceRepository.cs
@@ -6,17 +6,17 @@ namespace Chms.Application.Common.Interface.Repositories;
 public interface IIncomeRepository
 {
 
-    public Task<Guid> Create(Income entity);
+    public Task<long> Create(Income entity);
     public Task Update(Income entity);
-    public Income Get(int id);
-    public void Delete(int id);
-    public List<Income> List(FilterVm query);
+    public List<Income> Get(string incomeDate);
+    public void Delete(string incomeDate);
+    public List<ListVm> List(FilterVm query);
     public int TotalDataCount(FilterVm query);
 }
 
 public interface IExpenseRepository
 {
-    public Task<Guid> Create(Expense entity);
+    public Task<int> Create(Expense entity);
     public Task Update(Expense entity);
     public Expense Get(int id);
     public void Delete(int id);
