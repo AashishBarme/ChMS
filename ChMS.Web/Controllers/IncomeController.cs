e53ddc81a5b5ada41d665b05d5d610f348b2db6d
AashishBarme <aashish.barme9@gmail.com>
2024-09-07 23:25:57 +0545

    Added API for income

--- ChMS.Web/Controllers/IncomeController.cs
+++ ChMS.Web/Controllers/IncomeController.cs
@@ -6,6 +6,7 @@ using ChMS.Web.ViewModels;
 using Income = Chms.Domain.Entities.Income;
 using Chms.Domain.ViewModels.IncomeExpense;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.VisualBasic;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,7 +14,7 @@ namespace ChMS.Web.Controllers
 {
     [Route("[controller]")]
     [ApiController]
-    //[Authorize]
+    [Authorize]
     public class IncomeController : ControllerBase
     {
         public readonly IIncomeService _service;
@@ -28,27 +29,35 @@ namespace ChMS.Web.Controllers
         [HttpPost]
         public async Task<ActionResult<int>> Create([FromBody] AddIncome request)
         {
-           
-            if(request.Income.Count == 0 || String.IsNullOrEmpty(request.Date.ToString()))
+            try
             {
-                return Ok("Nothing found");
-            }
-
-            foreach(var income in request.Income)
-            {
-                Income entity = new()
+                if (request.Income.Count == 0 || String.IsNullOrEmpty(request.Date.ToString()))
                 {
-                    Category = income.Category,
-                    Amount = income.Amount,
-                    Date = request.Date,
-                    MemberId = income.MemberId,
-                    Description = income.Description,
-                    //CreatedBy = User.Identity.GetUserId()
-                };
-                await _service.Create(entity);
+                    return Ok("Nothing found");
+                }
+
+                foreach (var income in request.Income)
+                {
+                    Income entity = new()
+                    {
+                        Category = income.Category,
+                        Amount = income.Amount,
+                        IncomeDate = request.Date,
+                        MemberId = income.MemberId,
+                        Description = income.Description,
+                        //CreatedBy = User.Identity.GetUserId()
+                    };
+                    await _service.Create(entity);
+                }
+
+                return Ok();
+
             }
-            
-            return Ok();
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+           
         }
 
 
@@ -59,49 +68,82 @@ namespace ChMS.Web.Controllers
             {
                 return Ok("Nothing found");
             }
-
-            foreach (var income in request.Income)
+            try
             {
-                Income entity = new()
+                foreach (var income in request.Income)
                 {
-                    Id = income.Id,
-                    Category = income.Category,
-                    Amount = income.Amount,
-                    Date = request.Date,
-                    MemberId = income.MemberId,
-                    Description = income.Description
-                };
-                await _service.Update(entity);
+                    Income entity = new()
+                    {
+                        Id = income.Id,
+                        Category = income.Category,
+                        Amount = income.Amount,
+                        IncomeDate = request.Date,
+                        MemberId = income.MemberId,
+                        Description = income.Description,
+                        UpdatedDate = DateTime.Now,
+                    };
+                    await _service.Update(entity);
+                }
+
+                return Ok();
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{incomeDate}")]
+        public ActionResult Delete(string incomeDate)
+        {
+            if (String.IsNullOrEmpty(incomeDate.ToString()))
+            {
+                return BadRequest("Income Date is not selected");
+            }
+            try
+            {
+                _service.Delete(incomeDate);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
             }
 
-            return Ok();
         }
 
-        [HttpDelete("{id}")]
-        public ActionResult Delete(int id)
+        [HttpGet("{incomeDate}")]
+        public ActionResult Get(string incomeDate)
         {
-            _service.Delete(id);
-            return Ok();
-        }
-
-        [HttpGet("{id}")]
-        public ActionResult Get(int id)
-        {
-            return Ok(_service.Get(id));
+            if(String.IsNullOrEmpty(incomeDate.ToString()))
+            {
+                return BadRequest("Income Date is not selected");
+            }
+            try
+            {
+                return Ok(_service.Get(incomeDate));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpGet("list")]
         public ActionResult List([FromQuery] IncomeFilterQueryVm filterQuery)
         {
-            FilterVm filterVm = new(){
-                DateTime = filterQuery.Date,
-            };
+            var date = DateTime.Now;
+            var firstDayOfMonth = new DateTime(date.Year, date.Month, 1);
+            var lastDayOfMonth = firstDayOfMonth.AddMonths(1).AddDays(-1);
+            filterQuery.StartDate ??= firstDayOfMonth.ToString("yyyy-MM-dd");
+            filterQuery.EndDate ??= lastDayOfMonth.ToString("yyyy-MM-dd");
 
-            //MemberListResponseVm response = new(){
-            //    Items = _service.List(filterVm),
-            //    TotalDataCount = _service.TotalDataCount(filterVm)
-            //};
-            return Ok();
+            FilterVm filterVm = new(){
+                StartDate = filterQuery.StartDate,
+                EndDate = filterQuery.EndDate,
+            };
+            return Ok(_service.List(filterVm));
         }
     }
 }
--- ChMS.Web/ViewModels/Finance.cs
+++ ChMS.Web/ViewModels/Finance.cs
@@ -1,18 +1,20 @@
+using System.Numerics;
+
 namespace ChMS.Web.ViewModels
 {
     public class Income
     {
-        public int Id { get; set; }
+        public long Id { get; set; } = 0;
         public string? Category { get; set; }
         public int Amount { get; set; }
-        public DateTime Date { get; set; }
-        public int MemberId { get; set; } = 0;
+        public string? IncomeDate { get; set; }
+        public Guid MemberId { get; set; }
         public string? Description { get; set; }
     }
 
     public class AddIncome
     {
-        public DateTime Date { get; set; }
+        public string? Date { get; set; }
         public List<Income> Income { get; set; } = new();
     }
 
@@ -28,7 +30,8 @@ namespace ChMS.Web.ViewModels
 
     public class IncomeFilterQueryVm
     {
-        public DateTime Date { get; set; }
+        public string? StartDate { get; set; }
+        public string? EndDate { get; set; }
         public int Limit { get; set; } = 20;
         public int Offset { get; set; } = 0;
     }
